06bf22b83872fed781b0173234b2ebef685f10d9
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2023-11-16 18:02:09 +0100

    First

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turtorial</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <h3>TURTORIAL PHP</h3>
+
+    <!--
+        //input output
+
+        // instrukcja if
+
+        // pętla for i while
+
+        // operatory logiczne i arytmetyczne 
+
+
+        // incjalizacja zmiennych 
+
+        // tablica
+
+    -->
+</body>
+</html>
